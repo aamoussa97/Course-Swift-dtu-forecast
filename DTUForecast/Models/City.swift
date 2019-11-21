8e6dc2b3a759dcdf8208a972c851e13da1c03c96
AM <lgv2800@gmail.com>
2019-11-21 18:42:48 +0100

    Added model city.

---
+++ DTUForecast/Models/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  DTUForecast
+//
+//  Created by Ali Moussa on 21/11/2019.
+//  Copyright © 2019 SHAPE A/S. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct City: Hashable, Codable, Identifiable {
+    var id = Int
+    let name: String
+    let country: String
+}
---
+++ DTUForecast/Models/CityData.swift
@@ -0,0 +1,9 @@
+//
+//  CityData.swift
+//  DTUForecast
+//
+//  Created by Ali Moussa on 21/11/2019.
+//  Copyright © 2019 SHAPE A/S. All rights reserved.
+//
+
+import Foundation
